0ed2824583ffac4fbc2efe20de5178f4968a8d44
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-05 22:34:29 +0530

    Add intersection

--- lectures/03-Union-Intersection.ts
+++ lectures/03-Union-Intersection.ts
@@ -7,18 +7,16 @@ type numArrayUnionString = string | number[];
 let weather: numArrayUnionString = "Sunny";
 let temperateForecast: numArrayUnionString = [30, 32, 35, 31, 40];
 
-
 // The methods which are safe to call on this union type would be the methods available in both
 
-declare function getUnionType(): numArrayUnionString
+declare function getUnionType(): numArrayUnionString;
 
-const myUnionTypeInstance = getUnionType()
+const myUnionTypeInstance = getUnionType();
 
-myUnionTypeInstance.toUpperCase() // error
-myUnionTypeInstance.reduce(()=>{}) // error
-myUnionTypeInstance.slice() // works
-myUnionTypeInstance.length // works
-
+myUnionTypeInstance.toUpperCase(); // error
+myUnionTypeInstance.reduce(() => {}); // error
+myUnionTypeInstance.slice(); // works
+myUnionTypeInstance.length; // works
 
 // Example 2
 type Name = { name: string };
@@ -28,5 +26,32 @@ type NameUnionAge = Name | Age;
 
 let name: NameUnionAge = { name: "Sam" };
 let age: NameUnionAge = { age: 20 };
-let name_plus_age:NameUnionAge = { name: "Sam", age: 20 };
-let name_plus_age_plus_height:NameUnionAge = { name: "Sam", age: 20, height: 180 }; // error 
+let name_plus_age: NameUnionAge = { name: "Sam", age: 20 };
+let name_plus_age_plus_height: NameUnionAge = {
+  name: "Sam",
+  age: 20,
+  height: 180,
+}; // error
+
+// Intersection
+
+// Example 1
+
+type User = {
+  name: string;
+  age: number;
+};
+
+type AccessAllowed = {
+  access: true;
+};
+
+type Admin = User & AccessAllowed;
+
+const admin: Admin = {
+  name: "Sam",
+  age: 20,
+  access: true,
+};
+
+

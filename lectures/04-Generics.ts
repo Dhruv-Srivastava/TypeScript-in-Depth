b02dbaf0050786df913d6f4a43203db87e67a470
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-05-11 23:45:04 +0530

    Add example 1

---
+++ lectures/04-Generics.ts
@@ -0,0 +1,11 @@
+// Example 1
+function returnLastElement<T>(arr:T[]){
+    return arr[arr.length-1]
+}
+
+const names = ["Dhruv", "Sheel", "Kunal", "Kriti"]
+console.log(returnLastElement(names))
+
+const ratings = [4.5, 2, 1, 1.9, 3.2]
+console.log(ratings)
+
